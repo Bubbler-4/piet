2051f36cabeadf5df91cc90fa225605211ccd596
Bubbler-4 <bubbler9903@gmail.com>
2023-03-06 06:49:14 +0000

    Try 4

--- functions/api.svg.js
+++ functions/api.svg.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export function onRequest(context) {
   console.log(context.request.url.split('?')[1]);
-  return new Response('<svg></svg>');
+  const response = new Response('<svg></svg>');
+  response.headers.set('Content-Type', 'image/svg+xml');
+  return response;
 }
